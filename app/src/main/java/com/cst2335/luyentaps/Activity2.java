1e60833e2e0cff797b797099965c3c53b96179b7
vanguyen333 <96743036+vanguyen333@users.noreply.github.com>
2022-06-29 15:15:28 -0400

    when in activity2, the button can send back to activity 1

--- app/src/main/java/com/cst2335/luyentaps/Activity2.java
+++ app/src/main/java/com/cst2335/luyentaps/Activity2.java
@@ -2,13 +2,30 @@ package com.cst2335.luyentaps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity2 extends AppCompatActivity {
 
+    Button btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
-    }
-}
+        btn2 = findViewById(R.id.btnactivity2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backtoActivity1();
+            }
+            public void backtoActivity1(){
+                Intent intent = new Intent(Activity2.this, Activity1.class);
+                startActivity(intent);
+            }
+
+        });
+
+    }//end of onCreate
+}//end of class
